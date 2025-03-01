663b472f7513a9cbf907abf1d83136677a6dd2e4
Landon Davidson <accounts@landond.tech>
2025-03-01 12:06:42 -0800

    Add: rough draft

--- DotProduct.java
+++ DotProduct.java
@@ -9,19 +9,37 @@ public class DotProduct {
     // Your implementation must have linear work and log(n) span
     public static int dotProduct(int[] a, int[]b, int cutoff){
         DotProduct.CUTOFF = cutoff;
-        return POOL.invoke(new DotProductTask()); // TODO: add parameters to match your constructor
+        return POOL.invoke(new DotProductTask(0, a.length, a, b));
     }
 
     private static class DotProductTask extends RecursiveTask<Integer>{
-        // TODO: select fields
+        int[] a;
+        int[] b;
 
-        public DotProductTask(){
-            // TODO: implement constructor
+        public DotProductTask(int lo, int hi, int[] a, int[] b){
+            // Create a subarray of a and b from lo to hi
+            this.a = new int[hi - lo];
+            this.b = new int[hi - lo];
+            for (int i = lo; i < hi; i++) {
+                this.a[i - lo] = a[i];
+                this.b[i - lo] = b[i];
+            }
         }
 
         public Integer compute(){
-            //TODO: Implement.
-            return 0;
+            // If we are at the cutoff then sequentially process
+            if (a.length < CUTOFF) {
+                return Sequential.dotProduct(a, b);
+            }
+            // Calculate mid point and create left/right tasks around it
+            int mid = a.length / 2;
+            DotProductTask left = new DotProductTask(0, mid, a, b);
+            DotProductTask right = new DotProductTask(mid, a.length, a, b);
+            // Fork left and then compute right in this thread
+            left.fork();
+            int right_sum = right.compute();
+            int left_sum = left.join();
+            return right_sum + left_sum;
         }
     }
     
