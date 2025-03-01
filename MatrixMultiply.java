3ccd4bf8f85988b3fa9a8d71469894181a008974
Landon Davidson <accounts@landond.tech>
2025-03-01 12:57:20 -0800

    Add: matrix dot product implementation. Converts 2d to 1d array and copies DotProduct

--- MatrixMultiply.java
+++ MatrixMultiply.java
@@ -19,7 +19,14 @@ public class MatrixMultiply {
     // Your implementation must have linear work and log(n) span
     public static int dotProduct(int[][] a, int[][] b, int row, int col, int cutoff){
         MatrixMultiply.CUTOFF = cutoff;
-        return POOL.invoke(new DotProductTask()); // TODO: add parameters to match your constructor
+        // Grab the one dimensional row from 2d a
+        int[] ina = a[row];
+        // Copy each column value from 2d b into 1d array
+        int[] inb = new int[ina.length];
+        for (int i = 0; i < inb.length; i++) {
+            inb[i] = b[i][col];
+        }
+        return POOL.invoke(new DotProductTask(0, ina.length, ina, inb, cutoff));
     }
 
     private static class MatrixMultiplyAction extends RecursiveAction{
@@ -35,18 +42,38 @@ public class MatrixMultiply {
 
     }
 
+    // Exact copy of DotProduct since dotProduct converts from 2d to 1d
     private static class DotProductTask extends RecursiveTask<Integer>{
-        // TODO: select fields
+        int[] a;
+        int[] b;
+        int CUTOFF;
 
-        public DotProductTask(){
-            // TODO: implement constructor
+        public DotProductTask(int lo, int hi, int[] a, int[] b, int cutoff){
+            this.CUTOFF = cutoff;
+            // Create a subarray of a and b from lo to hi
+            this.a = new int[hi - lo];
+            this.b = new int[hi - lo];
+            for (int i = lo; i < hi; i++) {
+                this.a[i - lo] = a[i];
+                this.b[i - lo] = b[i];
+            }
         }
 
         public Integer compute(){
-            // TODO: implement
-            return 0;
+            // If we are at the cutoff then sequentially process
+            if (a.length <= CUTOFF) {
+                return Sequential.dotProduct(a, b);
+            }
+            // Calculate mid point and create left/right tasks around it
+            int mid = a.length / 2;
+            DotProductTask left = new DotProductTask(0, mid, a, b, CUTOFF);
+            DotProductTask right = new DotProductTask(mid, a.length, a, b, CUTOFF);
+            // Fork left and then compute right in this thread
+            left.fork();
+            int right_sum = right.compute();
+            int left_sum = left.join();
+            return right_sum + left_sum;
         }
-
     }
     
 }
